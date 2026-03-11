d4c4e0f8a6e362b5fbbc1c279709f3bdbebf459e
Matej Grubišić <grubisic401@gmail.com>
2026-03-11 20:19:19 +0100

    fix[alot of fixes]: but now can't connect to profanitydatabase

--- ProfanityService/DataAccess/Models/Profanity.cs
+++ ProfanityService/DataAccess/Models/Profanity.cs
@@ -2,6 +2,7 @@ namespace ProfanityService.DataAccess.Models;
 
 public class Profanity
 {
+    public Guid Id  { get; set; }
     public string Word { get; set; } = string.Empty;
     public string? CreatedAt  { get; set; }
 }
